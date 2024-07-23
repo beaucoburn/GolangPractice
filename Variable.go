cd742e6f6c89180d6d6bed33e3dd072fb8da4faa
beaucoburn <douglas.coburn@gmail.com>
2024-07-23 14:49:38 -0600

    new file:   Variable.go

---
+++ Variable.go
@@ -0,0 +1,12 @@
+package main
+import("fmt")
+
+func main(){
+  var student1 string = "John"
+  var student2 = "Jane"
+  x := 2
+
+  fmt.Println(student1)
+  fmt.Println(student2)
+  fmt.Println(x)
+}
