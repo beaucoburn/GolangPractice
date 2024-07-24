12d7772dc11e44b2499e752d12f0a7772cc6247e
beaucoburn <douglas.coburn@gmail.com>
2024-07-24 10:07:00 -0600

    new file:   StringFormatting.go

---
+++ StringFormatting.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var txt = "Hello"
+
+	fmt.Printf("%s\n", txt)
+	fmt.Printf("%q\n", txt)
+	fmt.Printf("%8s\n", txt)
+	fmt.Printf("%-8s\n", txt)
+	fmt.Printf("%x\n", txt)
+	fmt.Printf("% x\n", txt)
+}
