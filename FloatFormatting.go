caf58e9fbbfd698f8bbd480c0693f1c9f7942a6f
beaucoburn <douglas.coburn@gmail.com>
2024-07-24 10:37:52 -0600

    new file:   FloatFormatting.go

---
+++ FloatFormatting.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i = 3.141
+
+	fmt.Printf("%e\n", i)
+	fmt.Printf("%f\n", i)
+	fmt.Printf("%.2f\n", i)
+	fmt.Printf("%6.2f\n", i)
+	fmt.Printf("%g\n", i)
+}
