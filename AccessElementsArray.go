4eaa229be760e603b18d1f50b6480ad5dfd0c490
beaucoburn <douglas.coburn@gmail.com>
2024-07-24 17:14:14 -0600

    new file:   AccessElementsArray.go 	new file:   ChangeElementsArray.go 	new file:   InitailizeSpecificElementsArray.go 	new file:   LengthArray.go

---
+++ AccessElementsArray.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	prices := [3]int{10, 20, 30}
+
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+}
---
+++ InitailizeSpecificElementsArray.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr1 := [5]int{1: 10, 2: 40}
+
+	fmt.Println(arr1)
+}
