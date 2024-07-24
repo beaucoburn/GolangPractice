35826a0d690b09275c05dd8b5bb3f5f391b05683
beaucoburn <douglas.coburn@gmail.com>
2024-07-24 06:24:01 -0600

    new file:   FormattedPrinting.go

---
+++ FormattedPrinting.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt")
+
+func main(){
+  var i string = "Hello"
+  var j int = 15
+
+  fmt.Printf("i has value: %v and type: %T\n", i, i)
+  fmt.Printf("j has value: %v and type: %T\n", j, j)
+}
