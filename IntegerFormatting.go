024fcdcb79e29b3bcb1274c9755518cff1567f34
beaucoburn <douglas.coburn@gmail.com>
2024-07-24 09:46:37 -0600

    new file:   IntegerFormatting.go

---
+++ IntegerFormatting.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i = 15
+
+	fmt.Printf("%b\n", i)
+	fmt.Printf("%d\n", i)
+	fmt.Printf("%+d\n", i)
+	fmt.Printf("%o\n", i)
+	fmt.Printf("%O\n", i)
+	fmt.Printf("%x\n", i)
+	fmt.Printf("%X\n", i)
+	fmt.Printf("%#x\n", i)
+	fmt.Printf("%4d\n", i)
+	fmt.Printf("%-4d\n", i)
+	fmt.Printf("%04d\n", i)
+
+}
