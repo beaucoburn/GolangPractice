6eb2edfdb033a632e206197e51cc22294b1119f1
beaucoburn <douglas.coburn@gmail.com>
2024-07-25 08:16:43 -0600

    new file:   SliceFromArray.go

---
+++ SliceFromArray.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr1 := [6]int{10, 11, 12, 13, 14, 15}
+	myslice := arr1[2:4]
+
+	fmt.Printf("myslice = %v\n", myslice)
+	fmt.Printf("length = %d\n", len(myslice))
+	fmt.Printf("capacity = %d\n", cap(myslice))
+}
