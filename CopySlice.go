f6c1beaab672485484ad0cc4221f29508cd47cb4
beaucoburn <douglas.coburn@gmail.com>
2024-07-25 14:57:18 -0600

    new file:   CopySlice.go

---
+++ CopySlice.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	fmt.Printf("numbers = %v\n", numbers)
+	fmt.Printf("length = %d\n", len(numbers))
+	fmt.Printf("capacity = %d\n", cap(numbers))
+
+	neededNUmbers := numbers[:len(numbers)-10]
+	numbersCopy := make([]int, len(neededNUmbers))
+	copy(numbersCopy, neededNUmbers)
+
+	fmt.Printf("numbersCopy = %v\n", numbersCopy)
+	fmt.Printf("length = %d\n", len(numbersCopy))
+	fmt.Printf("capacity = %d\n", cap(numbersCopy))
+}
