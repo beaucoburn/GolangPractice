32c3f45e297d29ffa89c82bb2e82a02c28257850
beaucoburn <douglas.coburn@gmail.com>
2024-07-25 12:34:04 -0600

    new file:   MakeSlice.go

---
+++ MakeSlice.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myslice1 := make([]int, 5, 10)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice2 := make([]int, 5)
+	fmt.Printf("myslice2 = %v\n", myslice2)
+	fmt.Printf("length = %d\n", len(myslice2))
+	fmt.Printf("capacity = %d\n", cap(myslice2))
+}
