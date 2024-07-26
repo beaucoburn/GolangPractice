26139c24901fee486946d2ee0403964dc498b1b9
beaucoburn <douglas.coburn@gmail.com>
2024-07-26 06:53:44 -0600

    new file:   IfElse.go

---
+++ Conditions/IfElse.go
@@ -0,0 +1,12 @@
+package main
+import ("fmt")
+
+func main() {
+  time := 20
+  fmt.Println("Time is", time)
+  if (time < 18){
+    fmt.Println("Good day.")
+  } else {
+    fmt.Println("Good evening.")
+  }
+}
