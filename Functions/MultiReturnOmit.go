f27c0100c6e9743b4e9a58d3cb756cae2c22965f
beaucoburn <douglas.coburn@gmail.com>
2024-07-27 12:49:00 -0600

    new file:   MultiReturnOmit.go

---
+++ Functions/MultiReturnOmit.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func myFunction(x int, y string) (result int, txt1 string) {
+	result = x + x
+	txt1 = y + " World!"
+	return
+}
+
+func main() {
+	_, b := myFunction(5, "Hello")
+	fmt.Println(b)
+}
