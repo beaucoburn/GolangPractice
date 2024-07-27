f08a74a91bf3652519122c062b21db4183aa3aba
beaucoburn <douglas.coburn@gmail.com>
2024-07-27 13:55:17 -0600

    new file:   MapWithMake.go

---
+++ MapWithMake.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = make(map[string]string)
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
+
+	b := make(map[string]int)
+	b["Oslo"] = 1
+	b["Bergen"] = 2
+	b["Trondheim"] = 3
+	b["Stavanger"] = 4
+
+	fmt.Printf("a\t%v\n", a)
+	fmt.Printf("b\t%v\n", b)
+}
