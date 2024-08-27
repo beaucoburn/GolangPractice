895f56ca0b106860e801e68d3ad8d17fd9a51533
beaucoburn <douglas.coburn@gmail.com>
2024-08-27 14:28:11 -0600

    modified:   Web/Forms/Forms.go

--- Web/Forms/Forms.go
+++ Web/Forms/Forms.go
@@ -12,7 +12,7 @@ type ContactDetails struct {
 }
 
 func main() {
-  tmpl := template.Must(template.ParseFiles("forms.html"))
+  tmpl := template.Must(template.ParseFiles("Forms.html"))
 
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
